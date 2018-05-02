b5f8d9d72f327da67fcdb3445aab3ec8de92501e
5CLASS-184 <5CLASS-184@BIT01-21>
2018-05-02 18:27:01 +0900

    Spring MySQL ConnectionTest 완료

---
+++ src/test/java/org/zerock/web/DatabaseConnectTests.java
@@ -0,0 +1,35 @@
+package org.zerock.web;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class DatabaseConnectTests {
+	
+	final String DRIVER ="com.mysql.jdbc.Driver";
+	final String URL ="jdbc:mysql://10.10.10.14:3306/jr01?useSSL=false";
+	
+	final String USER = "study";
+	final String PW ="study";
+	
+	
+	@Test
+	public void testConnection()throws Exception {
+		Class.forName(DRIVER);
+		
+		try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+			System.out.println(con);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}	
+		
+	}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
